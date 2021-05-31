985f267943ca77aa9589aa6a6857b658e79b0036
noemie cadet <noemie.cadet@epitech.eu>
2021-05-31 15:51:29 +0400

    exo2

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -3,9 +3,16 @@ import re
 with open("exo2.txt", "r") as file:
     txt = file.read()
 
-rgx = re.compile("(.* : \\{[^\\}]*\\})")
-res = rgx.findall(txt)
 
-for i in res:
-    print(i, end="")
-print()
+rgx = re.compile("(.* : \\{[^\\}]*\\})")
+cmd = rgx.findall(txt)
+
+rgx2 = re.compile("(\\#.*\n)")
+com = rgx2.findall(txt)
+
+res = re.sub("[*\n]", '', (re.sub(rgx2, '', (re.sub(rgx, '', txt)))))
+
+if len(res) != 0:
+    print("ERROR")
+else:
+    print(txt)
